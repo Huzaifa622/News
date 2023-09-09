094be620545d7b945681eb008c7d2344ec84099a
unknown <thuzaifa07@gmail.com>
2023-09-09 22:02:15 +0500

    all

--- components/news/index.js
+++ components/news/index.js
@@ -2,12 +2,12 @@ import React, { useContext, useEffect, useReducer ,useState} from "react";
 import reducer from "@/pages/reducer";
 
 const API = 'https://newsapi.org/v2/everything?';
-const APIKey = process.env.API_KEY;
+const APIKey = process.env.NEXT_PUBLIC_API_KEY;
 console.log(APIKey)
 const AppContext = React.createContext();
 const initialState = {
   isLoading: true,
-  query: '',
+  query: '',    
   totalResults: 0,
   articles : [],
   page : 1
@@ -19,22 +19,39 @@ const AppProvider = ({children}) => {
 
   
 
-    const fetchApiData = async (url) =>{
+    const fetchApiData = async (url) => {
+      dispatch({
+        type: "GET_LOADING"
+      });
+    
+      try {
+        const res = await fetch(url);
+    
+        if (!res.ok) {
+          // Handle specific HTTP error status codes here
+          if (res.status === 401) {
+            throw new Error('Unauthorized - Check your API key');
+          } else if (res.status === 404) {
+            throw new Error('Not found - The requested resource does not exist');
+          } else {
+            throw new Error('Server Error - Something went wrong');
+          }
+        }
+    
+        const data = await res.json();
+    
         dispatch({
-          type: "GET_LOADING"
-        })
-
-       const res = await fetch(url);
-       const data = await res.json();
-       
-       dispatch({
-       type: 'GET_NEWS',
-       payload: {
-        articles: data.articles,
-        totalResults: data.totalResults
-      },
-       })
-    }
+          type: 'GET_NEWS',
+          payload: {
+            articles: data.articles,
+            totalResults: data.totalResults
+          },
+        });
+      } catch (error) {
+        console.error('Error:', error.message);
+        // Optionally, you can dispatch an error action or show an error message to the user
+      }
+    };
 
     const searchNews = (searchUpdate) =>{
       dispatch({
@@ -51,8 +68,11 @@ const AppProvider = ({children}) => {
         if (myQuery === '') {
           myQuery = 'tesla';
         }
+
+
         fetchApiData(`${API}q=${myQuery}&apiKey=${APIKey}`);
       }, 1000);
+      console.log(APIKey)
     return ()=> clearTimeout(timeOut);
     }, [state.query,state.page])
     
